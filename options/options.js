24bc22452fbf7c21160caed9a9099625fce465b1
Crowdbotics <team@crowdbotics.com>
2023-11-29 22:59:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "anonymousconnect-44804",
+  url: "https://anonymousconnect-44804.botics.co",
+  api: "https://anonymousconnect-44804.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
